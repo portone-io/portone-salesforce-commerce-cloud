ca59016d7b256d3ce22829099a0d0ce52e5f2978
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-13 09:50:34 +0000

    feat(VSIMP-003): extend a controller

--- cartridges/int_import_sfra/cartridge/controllers/Checkout.js
+++ cartridges/int_import_sfra/cartridge/controllers/Checkout.js
@@ -1,1 +1,18 @@
-// checkout controller
+'use strict';
+
+const server = require('server');
+server.extend(module.superModule);
+
+/**
+ * Checkout-Begin : The Checkout-Begin endpoint will render the checkout shipping page for both guest shopper and returning shopper
+ * @extend Base/Checkout-Begin
+ * @function
+ * @memberof Checkout
+ */
+server.append('Begin', function (req, res, next) {
+	let viewData = res.getViewData();
+
+	next();
+});
+
+module.exports = server.exports();
