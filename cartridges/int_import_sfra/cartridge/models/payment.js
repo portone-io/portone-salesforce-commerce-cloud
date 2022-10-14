f083ce8a3d5becf936f64fcbe728927d717fb8cb
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-14 03:42:23 +0000

    feat(VSIMP-003): validate the payment methods against the payment gateway

--- cartridges/int_import_sfra/cartridge/models/payment.js
+++ cartridges/int_import_sfra/cartridge/models/payment.js
@@ -2,30 +2,58 @@
 
 const Site = require('dw/system/Site');
 const IamportConstants = require('*/cartridge/constants/iamportConstants');
-const pgMapping = require('*/cartridge/config/pgMapping');
+const pgValidators = require('*/cartridge/config/pgValidators');
 
 const basePayment = module.superModule;
 
 /**
- * 
- * @param {*} paymentGateway 
- * @param {*} paymentMethod 
+ * Validate the selected payment methods against the actual ones specified in the PG validators
+ *
+ * @param {Array<dw.value.EnumValue>} selectedPaymentMethods - The selected payment methods in SitePreferences
+ * @param {Array<Object>} actualPaymentMethods - The actual payment methods in the PG validators
+ * @return {Array<Object>} - The valid payment methods
  */
-function validatePaymentMethods() {
+function validatePaymentMethods(selectedPaymentMethods, actualPaymentMethods) {
+	// eslint-disable-next-line array-callback-return, consistent-return
+	let validPaymentMethods = selectedPaymentMethods.filter(function (selectedPaymentMethod) {
+		let validPaymentMethod;
 
+		for (let i = 0; i < actualPaymentMethods.length; i += 1) {
+			let actualPaymentMethod = actualPaymentMethods[i];
+
+			if (selectedPaymentMethod.value === actualPaymentMethod.id) {
+				validPaymentMethod = selectedPaymentMethod;
+				break;
+			}
+		}
+
+		if (validPaymentMethod) {
+			return validPaymentMethod;
+		}
+	});
+
+	return validPaymentMethods;
 }
 
 /**
  * Retrieve payment methods supported by the specified payment gateway
  *
+ * @param {string} paymentGatewayID - ID of the selected payment gateway
  * @returns {Array<Object>} - List of the payment methods
  */
-function getPaymentMethods() {
-	let pgMap = pgMapping[this.iamportPaymentGateway];
-	let actualPaymentMethods = pgMap.paymentMethods;
+function getPaymentMethods(paymentGatewayID) {
+	let paymentGateway = pgValidators[paymentGatewayID];
 
-	let selectedPaymentMethods = Site.getCurrent().getCustomPreferenceValue(IamportConstants.);
-	
+	TODO: // Use logger here
+	if (empty(paymentGateway)) return [];
+
+	let customSitePrefAttributes = Site.getCurrent().getPreferences().getCustom();
+	let selectedPaymentMethods = customSitePrefAttributes[paymentGateway.paymentMethodsAttributeID];
+
+	let actualPaymentMethods = paymentGateway.paymentMethods;
+	let paymentMethods = validatePaymentMethods(selectedPaymentMethods, actualPaymentMethods);
+
+	return paymentMethods;
 }
 
 /**
@@ -42,7 +70,7 @@ function Payment(currentBasket, currentCustomer, countryCode) {
 		|| IamportConstants.PG_FALLBACK;
 
 	this.iamportPaymentGateway = paymentGateway;
-	this.iamportPaymentMethods = getPaymentMethods();
+	this.iamportPaymentMethods = getPaymentMethods(paymentGateway);
 }
 
 module.exports = Payment;
