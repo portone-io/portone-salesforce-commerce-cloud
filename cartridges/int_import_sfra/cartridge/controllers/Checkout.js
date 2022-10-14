0f666a237a3c2113142dff04d87f3880901b9921
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-14 09:15:23 +0000

    feat(VSIMP-003): add dummy payment data

--- cartridges/int_import_sfra/cartridge/controllers/Checkout.js
+++ cartridges/int_import_sfra/cartridge/controllers/Checkout.js
@@ -10,7 +10,31 @@ server.extend(module.superModule);
  * @memberof Checkout
  */
 server.append('Begin', function (req, res, next) {
+	const preferences = require('*/cartridge/config/preferences');
+	const iamportConstants = require('*/cartridge/constants/iamportConstants');
+	const Site = require('dw/system/Site');
+
 	let viewData = res.getViewData();
+
+	// TODO: the paymentInformation must be moved to a hook
+	Object.assign(viewData, {
+		paymentInformation: {
+			pg: 'html5_inicis',
+			pay_method: 'card',
+			merchant_uid: 'ORD20180131-0000011',
+			name: 'Norway swivel chair',
+			amount: 100,
+			buyer_email: 'johndoe@gmail.com',
+			buyer_name: 'John Doe',
+			buyer_tel: '010-4242-4242',
+			buyer_addr: 'Shinsa-dong, Gangnam-gu, Seoul',
+			buyer_postcode: '01181'
+		},
+		merchantID: Site.getCurrent().getCustomPreferenceValue(iamportConstants.PG_MID_ATTRIBUTE_ID),
+		useIamportSFRA6: preferences.SFRA6_ENABLED
+	});
+
+	res.setViewData(viewData);
 
 	next();
 });
