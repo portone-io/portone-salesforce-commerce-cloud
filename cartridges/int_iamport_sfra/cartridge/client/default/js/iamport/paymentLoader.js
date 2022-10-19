ecc4ca1ea693a5bfccd287d1b4dd25051424f95b
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-19 10:54:29 +0000

    feat(VSIMP-003): make a request with payment info for validation

--- cartridges/int_iamport_sfra/cartridge/client/default/js/iamport/paymentLoader.js
+++ cartridges/int_iamport_sfra/cartridge/client/default/js/iamport/paymentLoader.js
@@ -8,13 +8,57 @@ const iamportUtilities = require('../scripts/utils/common');
 const IAMPORT_ARGS = { MID: $('input[name="merchantID"]').val().toString().replace(/['"]+/g, '') };
 
 /**
+ * Send payment information to the server for validation
+ *
+ * @param {Object} paymentInformation - Payment information response upon payment request
+ * @param {Object} paymentOptions - Additional payment information
+ */
+function sendPaymentInformation(paymentInformation, paymentOptions) {
+	jQuery.ajax({
+		url: paymentOptions.validationUrl,
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		data: paymentInformation,
+		success: function (data) {
+			let redirect = $('<form>')
+				.appendTo(document.body)
+				.attr({
+					method: 'POST',
+					action: data.continueUrl
+				});
+
+			$('<input>')
+				.appendTo(redirect)
+				.attr({
+					name: 'orderID',
+					value: data.orderID
+				});
+
+			$('<input>')
+				.appendTo(redirect)
+				.attr({
+					name: 'orderToken',
+					value: data.orderToken
+				});
+
+			redirect.submit();
+		},
+		error: function () {
+			// enable the placeOrder button here
+			$('body').trigger('checkout:enableButton', $('.next-step-button button'));
+		}
+	});
+}
+
+/**
  * Request payment to Iamport server using the payment information
  *
- * @param {*} item Iamport global object
- * @param {*} paymentInformation The payment information
+ * @param {string} item Iamport global object
+ * @param {Object} paymentResources The payment resources
+ * @param {string} validationUrl  the url to validate payment and place order
  */
-const requestPayment = function requestPayment(item, paymentInformation) {
-	if (paymentInformation) {
+const requestPayment = function requestPayment(item, paymentResources, validationUrl) {
+	if (paymentResources) {
 		let IMP = window[item];
 
 		if (!IMP || !IAMPORT_ARGS.MID) {
@@ -22,22 +66,36 @@ const requestPayment = function requestPayment(item, paymentInformation) {
 		}
 
 		IMP.init(IAMPORT_ARGS.MID);
-		IMP.request_pay(paymentInformation, function (paymentResponse) {
+		IMP.request_pay(paymentResources, function (paymentResponse) {
+			let paymentOptions = {
+				validationUrl: validationUrl
+			};
+
 			if (paymentResponse.success) {
-				// success
+				console.log('success');
+				sendPaymentInformation({
+					imp_uid: paymentResponse.imp_uid,
+					merchant_uid: paymentResponse.merchant_uid
+				}, paymentOptions);
 			} else {
 				// TODO: use a toast instead
 				iamportUtilities.createErrorNotification(paymentResponse.error_msg);
 			}
+
+			// TODO: Test only; remove it later
+			sendPaymentInformation({
+				imp_uid: paymentResponse.imp_uid || '',
+				merchant_uid: paymentResponse.merchant_uid || ''
+			}, paymentOptions);
 		});
 	}
 };
 
 module.exports = {
-	generalPayment: function (paymentInformation) {
+	generalPayment: function (paymentResources, validationUrl) {
 		try {
 			$.spinner().start();
-			deferLoader.defer('IMP', requestPayment, paymentInformation);
+			deferLoader.defer('IMP', requestPayment, paymentResources, validationUrl);
 		} catch (err) {
 			// TODO: handle errors with meaningful error messages
 			iamportUtilities.createErrorNotification(err.message);
