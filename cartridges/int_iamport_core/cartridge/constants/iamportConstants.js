2acb1377d56442a36e627fcf6a64a65442f54d88
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-19 08:27:22 +0000

    feat(VSIMP-003): add test amount to constants

--- cartridges/int_iamport_core/cartridge/constants/iamportConstants.js
+++ cartridges/int_iamport_core/cartridge/constants/iamportConstants.js
@@ -4,5 +4,6 @@ module.exports = {
 	PG_ATTRIBUTE_ID: 'iamport_paymentGateway',
 	PG_DEFAULT_FALLBACK: 'html5_inicis',
 	PG_MID_ATTRIBUTE_ID: 'iamport_merchantID',
-	DEFAULT_PAYMENT_METHOD: 'card'
+	DEFAULT_PAYMENT_METHOD: 'card',
+	TEST_AMOUNT: 100
 };
