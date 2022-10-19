7f58f5f970b98067d08721daee587091d769f5e2
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-19 06:20:43 +0000

    feat(VSIMP-003): extend the payment instrument controller to have the selected payment method

--- cartridges/int_iamport_sfra/cartridge/models/payment.js
+++ cartridges/int_iamport_sfra/cartridge/models/payment.js
@@ -57,14 +57,41 @@ function getPaymentMethods(paymentGatewayID) {
 }
 
 /**
+ * Extend the payment instrument with the payment option from the storefront payment method selection
+ *
+ * @param {Array<Object>} paymentInstruments - List of payment instruments
+ * @param {string} paymentOption - Iamport payment option chosen on the storefront
+ * @returns {Array<Object>} Payment instrument with the payment option parameter
+ */
+function extendPaymentInstrument(paymentInstruments, paymentOption) {
+	let results = [];
+
+	paymentInstruments.forEach(function (paymentInstrument) {
+		let result = paymentInstrument;
+
+		if (paymentInstrument.paymentMethod === 'Iamport') {
+			result.paymentOption = paymentOption;
+		}
+		results.push(result);
+	});
+
+	return results;
+}
+
+/**
  * Payment class that represents payment information for the current basket
  * @param {dw.order.Basket} currentBasket - the target Basket object
  * @param {dw.customer.Customer} currentCustomer - the associated Customer object
  * @param {string} countryCode - the associated Site countryCode
+ * @param {string} paymentOption - Iamport payment option
  * @constructor
  */
-function Payment(currentBasket, currentCustomer, countryCode) {
+function Payment(currentBasket, currentCustomer, countryCode, paymentOption) {
 	basePayment.call(this, currentBasket, currentCustomer, countryCode);
+
+	let paymentInstruments = this.selectedPaymentInstruments;
+	this.selectedPaymentInstruments = paymentInstruments && paymentOption
+		? extendPaymentInstrument(paymentInstruments, paymentOption) : null;
 
 	let paymentGateway = Site.getCurrent().getCustomPreferenceValue(IamportConstants.PG_ATTRIBUTE_ID)
 		|| IamportConstants.PG_DEFAULT_FALLBACK;
