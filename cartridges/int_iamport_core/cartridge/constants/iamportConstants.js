7bf09f00ae97e3e0a684c73e73dc8ff906ab2eff
nx-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-10-19 06:17:04 +0000

    feat(VSIMP-003): add a default payment method

--- cartridges/int_iamport_core/cartridge/constants/iamportConstants.js
+++ cartridges/int_iamport_core/cartridge/constants/iamportConstants.js
@@ -3,5 +3,6 @@
 module.exports = {
 	PG_ATTRIBUTE_ID: 'iamport_paymentGateway',
 	PG_DEFAULT_FALLBACK: 'html5_inicis',
-	PG_MID_ATTRIBUTE_ID: 'iamport_merchantID'
+	PG_MID_ATTRIBUTE_ID: 'iamport_merchantID',
+	DEFAULT_PAYMENT_METHOD: 'card'
 };
