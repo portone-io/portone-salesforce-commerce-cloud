3d6d4ee40990a330dd4d5eba32b0f78d205c6d75
andreina-proyectos <andreina.romero@viseo.com>
2022-11-06 20:12:16 +0100

    Add confirmation email function to custom checkout helper

--- cartridges/int_iamport_sfra/cartridge/scripts/checkout/checkoutHelpers.js
+++ cartridges/int_iamport_sfra/cartridge/scripts/checkout/checkoutHelpers.js
@@ -46,7 +46,35 @@ function recreateCurrentBasket(order, subject, reason) {
 	}
 }
 
+/**
+ * Sends a confirmation to the current user
+ * @param {dw.order.Order} order - The current user's order
+ * @param {string} locale - the current request's locale id
+ * @returns {void}
+ */
+function sendConfirmationEmail(order, locale) {
+	var OrderModel = require('*/cartridge/models/order');
+	var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
+	var Locale = require('dw/util/Locale');
+
+	var currentLocale = Locale.getLocale(locale);
+
+	var orderModel = new OrderModel(order, { countryCode: currentLocale.country, containerView: 'order' });
+
+	var orderObject = { order: orderModel };
+
+	var emailObj = {
+		to: order.customerEmail,
+		subject: Resource.msg('subject.order.confirmation.email', 'order', null),
+		from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@testorganization.com',
+		type: emailHelpers.emailTypes.orderConfirmation
+	};
+
+	emailHelpers.sendEmail(emailObj, 'checkout/confirmation/confirmationEmail', orderObject);
+}
+
 module.exports = Object.assign(base, {
 	recreateCurrentBasket: recreateCurrentBasket,
-	addOrderNote: addOrderNote
+	addOrderNote: addOrderNote,
+	sendConfirmationEmail: sendConfirmationEmail
 });
