487adc7b44a671217566dc69d12bd144c9abdf6b
ks-isaacoppong-baah <isaac.oppong-baah@kcsit.pt>
2022-11-15 03:40:30 +0000

    fix: remove redundant code

--- cartridges/int_iamport_sfra/cartridge/scripts/checkout/checkoutHelpers.js
+++ cartridges/int_iamport_sfra/cartridge/scripts/checkout/checkoutHelpers.js
@@ -4,8 +4,8 @@ const BasketMgr = require('dw/order/BasketMgr');
 const OrderMgr = require('dw/order/OrderMgr');
 const Transaction = require('dw/system/Transaction');
 const base = module.superModule;
-var Resource = require('dw/web/Resource');
-var Site = require('dw/system/Site');
+const Resource = require('dw/web/Resource');
+const Site = require('dw/system/Site');
 
 /**
  * Recreate the current basket from an existing order
@@ -49,33 +49,6 @@ function recreateCurrentBasket(order, subject, reason) {
 }
 
 /**
- * Send order confirmation email
- * @param {dw.order.Order} order - The current user's order
- * @param {string} locale - the current request's locale id
- * @returns {void}
- */
-function sendConfirmationEmail(order, locale) {
-	var OrderModel = require('*/cartridge/models/order');
-	var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
-	var Locale = require('dw/util/Locale');
-
-	var currentLocale = Locale.getLocale(locale);
-
-	var orderModel = new OrderModel(order, { countryCode: currentLocale.country, containerView: 'order' });
-
-	var orderObject = { order: orderModel };
-
-	var emailObj = {
-		to: order.customerEmail,
-		subject: Resource.msg('subject.order.confirmation.email', 'order', null),
-		from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@testorganization.com',
-		type: emailHelpers.emailTypes.orderConfirmation
-	};
-
-	emailHelpers.sendEmail(emailObj, 'checkout/confirmation/confirmationEmail', orderObject);
-}
-
-/**
  * Send order cancellation email
  * @param {dw.order.Order} order - The current user's order
  * @param {string} locale - the current request's locale id
@@ -105,6 +78,5 @@ function sendPaymentOrderCancellationEmail(order, locale) {
 module.exports = Object.assign(base, {
 	recreateCurrentBasket: recreateCurrentBasket,
 	addOrderNote: addOrderNote,
-	sendConfirmationEmail: sendConfirmationEmail,
 	sendPaymentOrderCancellationEmail: sendPaymentOrderCancellationEmail
 });
