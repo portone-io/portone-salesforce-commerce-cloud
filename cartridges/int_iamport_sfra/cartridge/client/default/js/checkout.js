e68340a5415a3a869aadfb91a70cf405036c7fe1
shivam varshney <shivam.varshney@viseo.com>
2023-02-22 20:21:19 +0800

    Commit for SFRA6

--- cartridges/int_iamport_sfra/cartridge/client/default/js/checkout.js
+++ cartridges/int_iamport_sfra/cartridge/client/default/js/checkout.js
@@ -1,17 +1,7 @@
 'use strict';
 
-const processInclude = require('base/util');
+var processInclude = require('base/util');
 
-$(function () {
-	var sfra5Enabled = $('input[name="sfra5Enabled"]').val();
-
-	if (sfra5Enabled === 'true') {
-		processInclude(require('./checkout/checkoutSFRA5'));
-		return;
-	} else if (sfra5Enabled === 'false') {
-		// Comment next line when you are working with SFRA 5. Uncomment on SFRA6
-		// processInclude(require('./checkout/checkoutSFRA6'));
-		return;
-	}
-	return;
+$(document).ready(function () {
+    processInclude(require('./checkout/checkout'));
 });
