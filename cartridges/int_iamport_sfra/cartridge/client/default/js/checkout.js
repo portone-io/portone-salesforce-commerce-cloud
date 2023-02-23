1efe1a7e100959a16513d2a7f90e5926843f6f46
shivam varshney <shivam.varshney@viseo.com>
2023-02-23 12:35:18 +0800

    reverted changes

--- cartridges/int_iamport_sfra/cartridge/client/default/js/checkout.js
+++ cartridges/int_iamport_sfra/cartridge/client/default/js/checkout.js
@@ -2,7 +2,7 @@
 
 var processInclude = require('base/util');
 var sfra5Enabled = $('input[name="sfra5Enabled"]').val();
-var checkoutFile = sfra5Enabled === 'true' ? require('./checkout/checkoutSFRA5') : require('./checkout/checkout')
+var checkoutFile = sfra5Enabled === 'true' ? require('./checkout/checkoutSFRA5') : require('./checkout/checkoutSFRA6')
 $(document).ready(function () {
     processInclude(checkoutFile);
 });
